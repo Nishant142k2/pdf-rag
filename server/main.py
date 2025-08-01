c38cefcb97ff30885fa1d3eaf7822ae0071e20f4
Nishant <ayushnishantbro1@gmail.com>
2025-08-01 06:52:26 +0530

    added port

--- server/main.py
+++ server/main.py
@@ -3,6 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from middlewares.exception_handlers import catch_exception_middleware
 from routes.upload import router as upload_router
 from routes.chat import router as chat_router
+from dotenv import load_dotenv
+import os
+import uvicorn
+load_dotenv()
 app = FastAPI(title= "RAG API", description= "RAG API for PDF Reader", version= "1.0.0") 
 
 #CORS Setup
@@ -25,4 +29,7 @@ app.include_router(upload_router)
 app.include_router(chat_router)
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
